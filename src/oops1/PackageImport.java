1d3dacb7b6f43f3789664545ab9e73ebb8b5ce06
Achyuth <achyuththeajs@gmail.com>
2022-09-16 14:41:37 +0530

    commit1

---
+++ src/oops1/PackageImport.java
@@ -0,0 +1,11 @@
+package oops1;
+//import basics.*; //imports all classes of basics pack
+import basics.BiggestNo;
+public class PackageImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
